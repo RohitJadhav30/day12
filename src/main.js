0cac3386bdde9766f46e76fb4ef71a3a027a724e
Rohit Jadhav <jadhavrohit3004@gmail.com>
2023-06-10 15:37:12 +0530

    files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+import { MongoClient } from "mongodb";
+
+async function main(){
+    const uri = "mongodb://127.0.0.1:27017";
+
+    const client = new MongoClient(uri);
+
+    const db = client.db("mydb");
+    const message = db.collection("message");
+
+    await message.insertOne({"message" : "Rohit Jadhav"});
+
+    await client.close();
+    console.log("Record Added");
+}
+main();
